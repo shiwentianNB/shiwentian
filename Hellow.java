cbdae848b1c26b95a367a13dc96bebd0e00667b6
vwconfig <174967866@qq.com>
2017-12-21 14:04:54 +0800

    Update Hellow.java

--- Hellow.java
+++ Hellow.java
@@ -2,10 +2,5 @@ package shiwentian;
 public class Hellow{
 	public static void main(String [] args){
 		System.err.println(1);
-		System.err.println(4);
-		System.err.println(2);//
-		System.err.println(3);
-		System.err.println(666);
-		System.err.println(777);
 	}
 }
