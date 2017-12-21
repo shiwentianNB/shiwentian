ff447eda171d667502f6890ff21378eed7997e7e
shiwentian <wentian.shi@futuremove.cn>
2017-12-21 13:59:50 +0800

    .

--- Hellow.java
+++ Hellow.java
@@ -1,11 +1,6 @@
 package shiwentian;
 public class Hellow{
 	public static void main(String [] args){
-		System.err.println(1);
-		System.err.println(4);
-		System.err.println(2);//
-		System.err.println(3);
-		System.err.println(666);
-		System.err.println(777);
+
 	}
 }
