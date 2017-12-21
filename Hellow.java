c1c356db60b20a2cfa91dcd738239c1e6206edd8
vwconfig <174967866@qq.com>
2017-12-21 11:05:14 +0800

    Update Hellow.java

--- Hellow.java
+++ Hellow.java
@@ -2,11 +2,6 @@ package shiwentian;
 public class Hellow{
 	public static void main(String [] args){
 		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("4");
-		System.out.println("5");
-		System.out.println("7");
+	
 	}
 }
